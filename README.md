41d09cc20f710d1c43beb187d7991e45ad658907
Arijit Basu <sayanarijit@gmail.com>
2026-09-23 00:20:10 +0530

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# zenselection
+
+Hide the selection pane or dynamically adjust its length as you select paths.
+
+This plugin works with the default layout config. If you use a plugin that modified the default layout, you will need to pass the right vertical layout object, that contains `Selection` as its first element and possibly `HelpMenu` as the second explicitely.
+
+## Also See
+
+- [zentable.xplr](https://github.com/sayanarijit/zentable.xplr)
+- [treeview.xplr](https://github.com/sayanarijit/tree-view.xplr)
+- [dual-pane.xplr](https://github.com/sayanarijit/dual-pane.xplr)
+- [tri-pane.xplr](https://github.com/sayanarijit/tri-pane.xplr)
---
+++ init.lua
@@ -0,0 +1,65 @@
+---@diagnostic disable
+local xplr = xplr
+---@diagnostic enable
+
+local vertical_layout_config = nil
+
+local function render(ctx)
+  if vertical_layout_config == nil then
+    error("Vertical layout is not set up")
+  end
+  local cloned_config = xplr.util.clone(vertical_layout_config)
+  local selection_count = #ctx.app.selection
+  local maybe_helpmenu = cloned_config.splits[2]
+  if selection_count == 0 then
+    table.remove(cloned_config.config.constraints, 1)
+    table.remove(cloned_config.splits, 1)
+    if maybe_helpmenu and maybe_helpmenu == "HelpMenu" then
+      cloned_config.config.constraints[1] = { Min = 1 }
+    end
+  else
+    cloned_config.config.constraints[1] = {
+      Length = selection_count
+        + (cloned_config.config.vertical_margin or cloned_config.config.margin or 0)
+        + 2, -- for the border
+    }
+    if maybe_helpmenu and maybe_helpmenu == "HelpMenu" then
+      cloned_config.config.constraints[2] = { Min = 1 }
+    end
+  end
+
+  -- return { CustomParagraph = { body = xplr.util.to_yaml(cloned_layout) } }
+  return { CustomLayout = { Vertical = cloned_config } }
+end
+
+local function setup(config)
+  config = config or {}
+  config.layout = config.layout or xplr.config.layouts.builtin.default
+  config.render = config.render or render
+
+  local horizontal_layout = config.layout.Horizontal
+  if not horizontal_layout then
+    error("Layout does not have horizontal split")
+  end
+
+  local right_panes = horizontal_layout.splits[2]
+  if not right_panes or not right_panes.Vertical then
+    error("Right split is not a vertical split")
+  end
+
+  local vertical_splits = right_panes.Vertical.splits
+  local selection_pane = vertical_splits[1]
+  if not selection_pane or selection_pane ~= "Selection" then
+    error("Right vertical split does not have selection pane on top")
+  end
+
+  vertical_layout_config = right_panes.Vertical
+
+  config.layout.Horizontal.splits[2] = {
+    Dynamic = "custom.zenselection.render",
+  }
+
+  xplr.fn.custom.zenselection = { render = config.render }
+end
+
+return { setup = setup }
